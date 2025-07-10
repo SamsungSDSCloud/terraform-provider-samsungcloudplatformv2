805252d6095ee93a6aa07a960903f9e5285fe2bf
samsungscpdev <scpdev@samsung.com>
2025-07-10 16:03:17 +0900

    Update Provider v1.0.1

--- examples/data-sources/samsungcloudplatformv2_gslb_gslb/versions.tf
+++ examples/data-sources/samsungcloudplatformv2_gslb_gslb/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     samsungcloudplatformv2 = {
-      version = "1.0.0"
+      version = "1.0.1"
       source  = "SamsungSDSCloud/samsungcloudplatformv2"
     }
   }
